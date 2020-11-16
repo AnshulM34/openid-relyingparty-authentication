de1036c7255d32c9b88c99c527f8bf2c30b38421
me@ans <35987337+AnshulM34@users.noreply.github.com>
2020-11-16 15:51:14 +0100

    Add files via upload

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace openid.relyingparty.authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "unknown-authentication-scheme")]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                return new StatusCodeResult(StatusCodes.Status200OK);
+            }
+            catch (Exception e)
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("/SignOut")]
+        public async Task<ActionResult> ClearSession()
+        {
+            try
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync("unknown-authentication-scheme");
+                return new StatusCodeResult(StatusCodes.Status200OK);
+            }
+            catch (Exception e)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("/Authenticated")]
+        public ActionResult IsAuthenticated()
+        {
+            try
+            {
+                if (HttpContext.User.Identity.IsAuthenticated)
+                    return new StatusCodeResult(StatusCodes.Status200OK); ;
+                return new StatusCodeResult(StatusCodes.Status401Unauthorized);
+            }
+            catch (Exception e)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,148 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+
+namespace openid.relyingparty.authentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+                options.Cookie.Name = Configuration["ASHE_CONNECTION_COOKIES"];
+                options.SlidingExpiration = true;
+
+            })
+            .AddGoogle(options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.ClientId = "clientId";
+                options.ClientSecret = "secret";
+                options.Scope.Add("openid,profile");
+            })
+            .AddFacebook(options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.ClientId = "clientId";
+                options.ClientSecret = "secret";
+                options.Scope.Add("openid,profile");
+            })
+            .AddOpenIdConnect("unknown-authentication-scheme", options =>
+            {
+                options.ProtocolValidator.NonceLifetime = System.TimeSpan.FromMinutes(25);
+                options.RemoteAuthenticationTimeout = System.TimeSpan.FromMinutes(25);
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "https://unknown-suthority";
+                options.ClientId = "";
+                options.ClientSecret = "secret";
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                options.ResponseMode = OpenIdConnectResponseMode.Query;
+                options.SaveTokens = true;
+                options.RequireHttpsMetadata = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.CallbackPath = "";
+                options.Scope.Clear();
+                options.Scope.Add("openid,profile");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "groups",
+                    ValidateIssuer = true
+                };
+                options.Events.OnTicketReceived += async context =>
+                {
+                    var tokens = context.Properties.GetTokens().ToList();
+                    var accessToken = tokens.FirstOrDefault(x => x.Name == "access_token")?.Value;
+                    if (accessToken != null)
+                    {
+                        // get something to store in cookies along with access token
+                        context.Properties.StoreTokens(tokens);
+                    }
+                
+                    await Task.FromResult(0);
+                };
+                options.Events.OnRedirectToIdentityProvider += context =>
+                {
+                    var userlanguage = context.Request.Query["language"].ToString() == "en" ? "en" : "sp";
+                    context.ProtocolMessage.SetParameter("currentLanguage", userlanguage);
+                    return Task.FromResult(0);
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
